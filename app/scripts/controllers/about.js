bba27924f53795f17fc8f60d26f44d749225167f
Sudarshan jhawar <sudarshan.jhawar@oitcolitcaz.com>
2017-11-30 16:06:40 +0530

    Initial setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webSpeechApiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webSpeechApiApp
+ */
+angular.module('webSpeechApiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webSpeechApiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webSpeechApiApp
+ */
+angular.module('webSpeechApiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
